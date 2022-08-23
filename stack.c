abdd4a2150dfa49a5859370199c083caeec8b054
Vinícius Kappke <vinikappke@gmail.com>
2022-08-23 19:24:58 -0300

    Update stack.c

--- stack.c
+++ stack.c
@@ -11,57 +11,58 @@ enum Options
 typedef struct
 {
     int value;
+    int code;
 } Node;
 
 typedef struct
 {
     Node *array;
-    int arr_start, arr_end, start, end;
-} List;
+    int size, next;
+} Stack;
 
 /**
- * @brief Adds a Node to the end of the list
+ * @brief Adds a Node to the end of the stack
  * 
- * @param list the list to be added to
+ * @param stack the stack to be added to
  * @return int 1 if successful, 0 if unsuccessful
  */
-int append(List *list)
+int append(Stack *stack)
 {
-    if (list->end == list->arr_end)
+    if (stack->next > stack->size)
     {
         printf("\nLista cheia\n\n");
         return 0;
     }
 
     Node value;
-    printf("Digite o valor a ser inserido na posição %d: ", list->end + 2);
+    printf("Digite o valor a ser inserido: ");
     scanf("%d", &value.value);
 
-    list->array[++list->end] = value;
+    printf("Digite o código do valor: ");
+    scanf("%d", &value.code);
+
+    stack->array[stack->next] = value;
+    stack->next++;
 
     return 1;
 }
 
 /**
- * @brief Removes the last element of the list and returns it
+ * @brief Removes the last element of the stack and returns it
  * 
- * @param list List to be removed from
- * @return Last node of the list
+ * @param stack Stack to be removed from
+ * @return Last node of the stack
  */
-Node pop(List *list)
+int pop(Stack *stack)
 {
-    Node n = {-999};
-    if (list->start == list->end)
+    if (stack->next <= 0)
     {
         printf("Lista vazia\n");
-        return n;
+        return 0;
     }
 
-    Node value = list->array[list->end];
-    list->array[list->end] = n;
-    list->end--;
-
-    return value;
+    Node n = {-999, -999};
+    stack->array[--stack->next] = n;
 }
 
 int main()
@@ -72,36 +73,35 @@ int main()
 
     Node array[size - 1];
 
-    List list;
-    list.array = array;
-    list.end = list.start = list.arr_start = 0;
-    list.end--;
-    list.arr_end = sizeof(array) / sizeof(array[0]);
+    Stack stack;
+    stack.array = array;
+    stack.size = sizeof(array) / sizeof(array[0]);
+    stack.next = 0;
 
     int option = NONE;
     do {
         printf("Lista: \n");
-        for (int i = 0; i <= list.end; i++)
-            printf("%d\t", list.array[i].value);
-        printf("\n\n");
+        for (int i = 0; i < stack.next; i++)
+            printf("%d/%d\t", stack.array[i].value, stack.array[i].code);
+        printf("\n");
 
         printf("Digite a opção desejada:\n");
         printf("1 - Inserir\t");
         printf("2 - Remover\t");
-        printf("3 - Sair\n\n");
+        printf("3 - Sair\n");
 
         scanf("%d", &option);
 
-        printf("\n\n");
+        printf("\n");
 
         switch (option)
         {
             case APPEND:
-                append(&list);
+                append(&stack);
                 break;
 
             case POP:
-                pop(&list);
+                pop(&stack);
                 break;
             
             case QUIT:
